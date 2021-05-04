c6db195a0d71cac8e817af9958ee7f56b62abac6
ayselmehmetzade <ayselmehmetzade@hotmail.com>
2021-05-04 22:39:54 +0300

    first commit

---
+++ kampOdev/src/kampOdev/Main.java
@@ -0,0 +1,148 @@
+package kampOdev;
+
+import java.util.Iterator;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		int sayi = 10;
+//		String mesaj = "ögrenci sayisi ";
+//
+//		System.out.println(mesaj + sayi);
+
+//		int sayi=20;
+//		if(sayi<20) {
+//			System.out.println("Sayý 20den küçüktür");
+//		}else if(sayi==20) {
+//			System.out.println("Sayý 20ye eþittir");
+//		}else {
+//			System.out.println("Sayý 20den küçük deðildir");
+//		}
+
+//		int sayi1=20;
+//		int sayi2=25;
+//		int sayi3=2;
+//		
+//		int enBuyuk=sayi1;
+//		
+//		if(enBuyuk<sayi2) {
+//			enBuyuk=sayi2;
+//		}
+//		if(enBuyuk<sayi3) {
+//			enBuyuk=sayi3;
+//		}
+//		
+//		System.out.println("En buyuk sayý " + enBuyuk);
+
+//		char grade = 'C';
+//		switch (grade) {
+//		case 'A':
+//			System.out.println("Mükemmel");
+//			break;
+//		case 'B':
+//		case 'C':
+//			System.out.println("Çok güzel");
+//			break;
+//		case 'D':
+//			System.out.println("Fena deðil");
+//			break;
+//		case 'F':
+//			System.out.println("Kötü");
+//			break;
+//		default:
+//			System.out.println("Geçersiz");
+//
+//		}
+
+//		//for
+//		for (int i = 0; i < 10; i++) {
+//			System.out.println(i);
+//		}
+//		System.out.println("Döngü bitti");
+//		
+//		for (int i = 2; i < 10; i+=2) {
+//			System.out.println(i);
+//		}
+//		System.out.println("Döngü bitti");
+//		
+//		//while
+//		int i=1;		
+//		while (i<10) {
+//			System.out.println(i);
+//			i+=2;
+//		}
+//		System.out.println("while Döngü bitti");
+//		
+//		//do-while
+//		int j=1;
+//		do {
+//			System.out.println(j);
+//			j+=2;
+//		} while (j<10);
+//		System.out.println("while Döngü bitti");
+
+//		String ogrenci1="Engin";		
+//		String ogrenci2="Derin";
+//		String ogrenci3="Salih";
+//		
+//		String [] ogrenciler = {"Engin","Derin","Salih"};
+//		String [] ogrenciler1 = new String[5];
+//		ogrenciler1[0]="Aysel";
+//		ogrenciler1[1]="Dilara";
+//		ogrenciler1[2]="Esra";
+//		ogrenciler1[3]="Kübra";
+//		ogrenciler1[4]="Ceren";
+//		
+//		for(int i=0; i<ogrenciler.length; i++) {
+//			System.out.println(ogrenciler[i]);
+//		}
+//		System.out.println("********");
+//		
+//		for(int i=0; i<ogrenciler1.length; i++) {
+//			System.out.println(ogrenciler1[i]);
+//		}
+//		System.out.println("********");
+//		for (String ogrenci: ogrenciler1) {
+//			System.out.println(ogrenci);		
+//			
+//		}
+
+//		double[] myList = { 1.2, 1.3, 5.6, 7.8 };
+//		double total = 0;
+//		double max = myList[0];
+//
+//		for (double list : myList) {
+//			if (max < list) {
+//				max = list;
+//			}
+//			total = total + list;
+//			System.out.println(list);
+//		}
+//
+//		System.out.println("Toplam " + total);
+//		System.out.println("En büyük sayi " + max);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
