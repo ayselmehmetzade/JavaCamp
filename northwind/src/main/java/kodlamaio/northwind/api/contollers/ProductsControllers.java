9c50f7c0f2eeffdf110eb5a86eed65a4bcc83ba7
ayselmehmetzade <ayselmehmetzade@hotmail.com>
2021-05-09 22:58:01 +0300

    first commit

---
+++ northwind/src/main/java/kodlamaio/northwind/api/contollers/ProductsControllers.java
@@ -0,0 +1,29 @@
+package kodlamaio.northwind.api.contollers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.entities.concretes.Product;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductsControllers {
+	
+	private ProductService productService;
+	
+	public ProductsControllers(ProductService productService) {
+		super();
+		this.productService = productService;
+	}
+
+	@GetMapping("/getall")
+	public List<Product> getAll(){
+		return this.productService.getAll();
+	}
+
+}
+
