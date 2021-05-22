deeff53644a3d58851979944a6ee9d579a261486
ayselmehmetzade <ayselmehmetzade@hotmail.com>
2021-05-22 22:54:35 +0300

    22 mayıs commit 1

--- northwind/src/main/java/kodlamaio/northwind/api/contollers/ProductsControllers.java
+++ northwind/src/main/java/kodlamaio/northwind/api/contollers/ProductsControllers.java
@@ -1,5 +1,8 @@
 package kodlamaio.northwind.api.contollers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,23 +10,23 @@ import org.springframework.web.bind.annotation.RestController;
 import kodlamaio.northwind.business.abstracts.ProductService;
 import kodlamaio.northwind.entities.concretes.Product;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api/products")
 public class ProductsControllers {
-	
+
 	private ProductService productService;
 	
+	@Autowired
 	public ProductsControllers(ProductService productService) {
 		super();
 		this.productService = productService;
 	}
 
 	@GetMapping("/getall")
+	
 	public List<Product> getAll(){
 		return this.productService.getAll();
 	}
-
+	
+	
 }
-
--- northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -7,6 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Data;
+
 @Data
 @Entity
 @Table(name = "products")
@@ -16,14 +17,19 @@ public class Product {
 	@GeneratedValue
 	@Column(name = "product_id")
 	private int id;
+
 	@Column(name = "category_id")
 	private int categoryId;
+
 	@Column(name = "product_name")
 	private String productName;
+
 	@Column(name = "unit_price")
 	private double unitPrice;
+
 	@Column(name = "units_in_stock")
 	private short unitsInStock;
+
 	@Column(name = "quantity_per_unit")
 	private String quantityPerUnit;
 
